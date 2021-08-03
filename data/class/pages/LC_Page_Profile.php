3854b7fdaf2fa56bcd4f581e5b133516690af555
Seasoft <info@seasoft.jp.net>
2021-08-03 11:15:01 +0900

    CHAIN-40 プロフィールページ
    親: CHAIN-43 プロフィール
    
    以下項目は未実装である。
    - 画像

--- data/class/pages/LC_Page_Profile.php
+++ data/class/pages/LC_Page_Profile.php
@@ -35,65 +35,53 @@ class LC_Page_Profile extends LC_Page_Ex
      */
     public function action()
     {
-        $customer_id = 2;
+        $customer_id = $_REQUEST['customer_id'];
         $this->arrCustomer = SC_Helper_Customer_Ex::sfGetCustomerData($customer_id);
+        $this->arrCustomerProducts = $this->getCustomerProducts($customer_id);
+        $this->arrFavoriteProducts = $this->getFavoriteProducts($customer_id);
     }
 
     /**
-     * お気に入りを取得する
+     * 指定された会員の商品を取得する
      *
-     * LC_Page_Mypage_Favorite::lfGetFavoriteProduct() を元に実装
-     * @param mixed $customer_id
-     * @param LC_Page_Mypage_Favorite $objPage
-     * @access private
-     * @return array お気に入り商品一覧
+     * @return array
      */
-    public function lfGetFavoriteProduct($customer_id, &$objPage)
+    public function getCustomerProducts($customer_id)
     {
-        $objQuery       = SC_Query_Ex::getSingletonInstance();
-        $objProduct     = new SC_Product_Ex();
+        $objQuery = SC_Query_Ex::getSingletonInstance();
+        $objProduct = new SC_Product_Ex();
 
-        $objQuery->setOrder('f.create_date DESC');
-        $where = 'f.customer_id = ? and p.status = 1';
-        if (NOSTOCK_HIDDEN) {
-            $where .= ' AND EXISTS(SELECT * FROM dtb_products_class WHERE product_id = f.product_id AND del_flg = 0 AND (stock >= 1 OR stock_unlimited = 1))';
-        }
-        $arrProductId  = $objQuery->getCol('f.product_id', 'dtb_customer_favorite_products f inner join dtb_products p using (product_id)', $where, array($customer_id));
+        $objQuery->setWhere('alldtl.customer_id = ?', [$customer_id]);
+        $objQuery->andWhere('alldtl.del_flg = 0');
+        $objQuery->andWhere('alldtl.status = 1');
+        $objQuery->setOrder('alldtl.product_id DESC');
+        $objQuery->setLimit(10);
 
-        $objQuery       = SC_Query_Ex::getSingletonInstance();
-        $objQuery->setWhere($this->lfMakeWhere('alldtl.', $arrProductId));
-        $linemax        = $objProduct->findProductCount($objQuery);
+        $addCols = ['count_of_favorite'];
+        $arrProducts = $objProduct->lists($objQuery, [], $addCols);
 
-        $objPage->tpl_linemax = $linemax;   // 何件が該当しました。表示用
+        return $arrProducts;
+    }
 
-        // ページ送りの取得
-        $objNavi        = new SC_PageNavi_Ex($objPage->tpl_pageno, $linemax, SEARCH_PMAX, 'eccube.movePage', NAVI_PMAX);
-        $this->tpl_strnavi = $objNavi->strnavi; // 表示文字列
-        $startno        = $objNavi->start_row;
+    /**
+     * 指定された会員のお気に入り商品を取得する
+     *
+     * @return array
+     */
+    public function getFavoriteProducts($customer_id)
+    {
+        $objQuery = SC_Query_Ex::getSingletonInstance();
+        $objProduct = new SC_Product_Ex();
 
-        $objQuery       = SC_Query_Ex::getSingletonInstance();
-        //$objQuery->setLimitOffset(SEARCH_PMAX, $startno);
-        // 取得範囲の指定(開始行番号、行数のセット)
-        $arrProductId  = array_slice($arrProductId, $startno, SEARCH_PMAX);
+        $objQuery->setWhere('EXISTS(SELECT * FROM dtb_customer_favorite_products WHERE customer_id = ? AND product_id = alldtl.product_id)', [$customer_id]);
+        $objQuery->andWhere('alldtl.del_flg = 0');
+        $objQuery->andWhere('alldtl.status = 1');
+        $objQuery->setOrder('alldtl.product_id DESC');
+        $objQuery->setLimit(10);
 
-        $where = $this->lfMakeWhere('', $arrProductId);
-        $where .= ' AND del_flg = 0';
-        $objQuery->setWhere($where, $arrProductId);
-        $arrProducts = $objProduct->lists($objQuery);
+        $addCols = ['count_of_favorite'];
+        $arrProducts = $objProduct->lists($objQuery, [], $addCols);
 
-        //取得している並び順で並び替え
-        $arrProducts2 = array();
-        foreach ($arrProducts as $item) {
-            $arrProducts2[$item['product_id']] = $item;
-        }
-        $arrProductsList = array();
-        foreach ($arrProductId as $product_id) {
-            $arrProductsList[] = $arrProducts2[$product_id];
-        }
-
-        // 税込金額を設定する
-        SC_Product_Ex::setIncTaxToProducts($arrProductsList);
-
-        return $arrProductsList;
+        return $arrProducts;
     }
 }
