4bb6308f7fe7077759acbead86b404cfd6f64b79
kk580 <kk580923@gmail.com>
2021-09-25 16:47:19 +0900

    refs CHAIN-183 【マイページ】選択待ちのアイテム詳細ページ

--- data/class/pages/mypage/myitem/LC_Page_Mypage_Myitem_Status.php
+++ data/class/pages/mypage/myitem/LC_Page_Mypage_Myitem_Status.php
@@ -11,6 +11,9 @@ class LC_Page_Mypage_Myitem_Status extends LC_Page_AbstractMypage_Ex
     /** ページナンバー */
     public $tpl_pageno;
 
+    /** @var SC_FormParam フォームパラメーター */
+    public $objFormParam;
+
     /**
      * Page を初期化する.
      *
@@ -19,7 +22,7 @@ class LC_Page_Mypage_Myitem_Status extends LC_Page_AbstractMypage_Ex
     public function init()
     {
         parent::init();
-        $this->tpl_subtitle = '出品中アイテム一覧';
+        $this->tpl_subtitle = '選択待ちのアイテム';
         $this->tpl_mypageno = 'item-list';
         // 1ページあたりの件数
         $this->dispNumber = 10;
@@ -42,15 +45,89 @@ class LC_Page_Mypage_Myitem_Status extends LC_Page_AbstractMypage_Ex
      */
     public function action()
     {
-        $objCustomer = new SC_Customer_Ex();
+        // パラメーター管理クラス
+        $this->objFormParam = new SC_FormParam_Ex();
+        // パラメーター情報の初期化
+        $this->arrForm = $this->lfInitParam($this->objFormParam);
+        // プロダクトIDの正当性チェック
+        $objProduct = new SC_Product_Ex();
+        $product_id = $this->lfCheckProductId($this->objFormParam->getValue('admin'), $this->objFormParam->getValue('product_id'), $objProduct);
 
-        $customer_id = $objCustomer->getValue('customer_id');
+        $this->arrChainProduct = $this->getChainProductStatus($product_id, $this);
+    }
 
-        // ページ送り用
-        if (isset($_POST['pageno'])) {
-            $this->tpl_pageno = intval($_POST['pageno']);
+    /**
+     * プロダクトIDの正当性チェック
+     *
+     * @param string $admin_mode
+     * @param int $product_id
+     * @param SC_Product $objProduct
+     * @return integer
+     */
+    public function lfCheckProductId($admin_mode, $product_id, SC_Product $objProduct)
+    {
+        // 管理機能からの確認の場合は、非公開の商品も表示する。
+        if (isset($admin_mode) && $admin_mode == 'on' && SC_Utils_Ex::sfIsSuccess(new SC_Session_Ex(), false)) {
+            $include_hidden = true;
+        } else {
+            $include_hidden = false;
         }
-        $this->arrProducts = $this->getProducts($customer_id, $this);
+
+        if (!$objProduct->isValidProductId($product_id, $include_hidden)) {
+            SC_Utils_Ex::sfDispSiteError(PRODUCT_NOT_FOUND);
+        }
+
+        return $product_id;
+    }
+
+    /**
+     * パラメーター情報の初期化
+     *
+     * @param SC_FormParam $objFormParam
+     * @return array
+     */
+    public function lfInitParam(SC_FormParam &$objFormParam)
+    {
+        $objFormParam->addParam('商品ID', 'product_id', INT_LEN, 'n', array('EXIST_CHECK', 'ZERO_CHECK', 'NUM_CHECK', 'MAX_LENGTH_CHECK'));
+        // 値の取得
+        $objFormParam->setParam($_REQUEST);
+        // 入力値の変換
+        $objFormParam->convParam();
+        // 入力情報を渡す
+        return $objFormParam->getFormParamList();
+    }
+
+    /**
+     * @param $customer_id
+     * @return array
+     * @throws Exception
+     */
+    private function getChainProductStatus($product_id)
+    {
+        $objHelperApi = new SC_Helper_Api_Ex();
+        $objHelperApi->setMethod('GET');
+
+        $arrChainProductStatus = array();
+
+        $arrProducts = $this->getProducts($product_id, $this);
+        $objProduct = new SC_Product_Ex();
+        foreach ($arrProducts as $arrProduct) {
+            $objHelperApi->setUrl(API_URL . 'chain/find?' . 'id=' . $arrProduct['product_id']);
+            $result = json_decode($objHelperApi->exec(), true);
+            if (count($result) > 0) {
+                $objHelperApi->setUrl(API_URL . 'chain/' . $result[0]['id']);
+                $result = json_decode($objHelperApi->exec(), true);
+                foreach ($result["selection_edge_list"] as $chainList) {
+                    if ($chainList['source_id'] == $_GET['product_id']) {
+                        $arrChainProductStatus['source_product'] = $arrProduct;
+                        $arrChainProductStatus['target_product'][] = $objProduct->getDetail($chainList['target_id']);
+                        $arrChainProductStatus['progress_percent'] = $result['progress_percent'];
+                    }
+                }
+            }
+        }
+
+        return $arrChainProductStatus;
     }
 
     /**
@@ -60,31 +137,32 @@ class LC_Page_Mypage_Myitem_Status extends LC_Page_AbstractMypage_Ex
      * @access private
      * @return array 出品中アイテム商品一覧
      */
-    public function getProducts($customer_id)
+    public function getProducts($product_id)
     {
         // 出品中アイテム商品ID取得
         $arrProductId = array();
-        $arrListingProducts  = SC_Product_Ex::getListingProducts($customer_id);
-        foreach ($arrListingProducts as $arrListingProduct) {
-            $arrProductId[] = $arrListingProduct['product_id'];
-        }
+//        $arrListingProducts  = SC_Product_Ex::getListingProducts($customer_id);
+//        foreach ($arrListingProducts as $arrListingProduct) {
+//            //$arrProductId[] = $arrListingProduct['product_id'];
+//        }
+        $arrProductId[] = $product_id;
 
-        $objQuery       = SC_Query_Ex::getSingletonInstance();
+        $objQuery = SC_Query_Ex::getSingletonInstance();
         $objQuery->setWhere($this->lfMakeWhere('alldtl.', $arrProductId));
-        $objProduct     = new SC_Product_Ex();
-        $linemax        = $objProduct->findProductCount($objQuery);
+        $objProduct = new SC_Product_Ex();
+        $linemax = $objProduct->findProductCount($objQuery);
 
         $this->tpl_linemax = $linemax;   // 何件が該当しました。表示用
 
         // ページ送りの取得
-        $objNavi        = new SC_PageNavi_Ex($this->tpl_pageno, $linemax, $this->dispNumber, 'eccube.movePage', NAVI_PMAX);
+        $objNavi = new SC_PageNavi_Ex($this->tpl_pageno, $linemax, $this->dispNumber, 'eccube.movePage', NAVI_PMAX);
         $this->tpl_strnavi = $objNavi->strnavi; // 表示文字列
-        $startno        = $objNavi->start_row;
+        $startno = $objNavi->start_row;
 
-        $objQuery       = SC_Query_Ex::getSingletonInstance();
+        $objQuery = SC_Query_Ex::getSingletonInstance();
         //$objQuery->setLimitOffset($this->dispNumber, $startno);
         // 取得範囲の指定(開始行番号、行数のセット)
-        $arrProductId  = array_slice($arrProductId, $startno, $this->dispNumber);
+        $arrProductId = array_slice($arrProductId, $startno, $this->dispNumber);
 
         $where = $this->lfMakeWhere('', $arrProductId);
         $where .= ' AND del_flg = 0';
