5cf385a14d944e2db6643adc312754e4b5c10d9c
Seasoft <info@seasoft.jp.net>
2021-09-30 14:55:40 +0900

    CHAIN-208 Chainループの概要図に倣った初期データを用意する
    API
    - dtb_customer_favorite_products の内容を基に API 要求する。
    - 削除は行わない。
    - ChainAPI のエラー時の例外メッセージに具体的な内容を含むように調整している。
    
    呼び出し方
    $ docker exec web /bin/sh -c "sudo -u www-data -g www-data /var/www/data/batch/send_api_customer_favorite_products.php"

---
+++ data/batch/send_api_customer_favorite_products.php
@@ -0,0 +1,61 @@
+#!/usr/local/bin/php
+<?php
+ini_set('display_errors', '1');
+chdir(dirname(__FILE__));
+require_once '../../html/require.php';
+while (@ob_end_flush());
+
+echo "START\n";
+Batch::loop();
+// Batch::chain();
+echo "END\n";
+
+class Batch {
+    /**
+     * dtb_customer_favorite_products の内容を基に API 要求する。
+     */
+    public static function loop() {
+        $arrCustomerFavoriteProducts = self::getCustomerFavoriteProducts();
+        foreach ($arrCustomerFavoriteProducts as $arrRow) {
+            $objHelperApi = new SC_Helper_Api_Ex();
+            $objHelperApi->setUrl(API_URL . 'chain/edges/add');
+            $objHelperApi->setMethod('POST');
+            $data = [
+                "source_id" => $arrRow['product_id'],
+                "target_id" => $arrRow['target_id'],
+                "date" => str_replace('+00:00', 'Z', gmdate('c', strtotime($arrRow['update_date']))),
+            ];
+            $objHelperApi->setPostParam($data);
+            $result_raw = $objHelperApi->exec();
+            $result = json_decode($result_raw, null, 512, JSON_THROW_ON_ERROR);
+            var_dump($result);
+        }
+    }
+
+    /**
+     * loop() と供に実行することで、Chain を成立させる。
+     */
+    public static function chain() {
+        $objHelperApi = new SC_Helper_Api_Ex();
+        $objHelperApi->setUrl(API_URL . 'chain/edges/add');
+        $objHelperApi->setMethod('POST');
+        $data = [
+            "source_id" => 2,
+            "target_id" => 1,
+            "date" => str_replace('+00:00', 'Z', gmdate('c')),
+        ];
+        $objHelperApi->setPostParam($data);
+        $result_raw = $objHelperApi->exec();
+        $result = json_decode($result_raw, null, 512, JSON_THROW_ON_ERROR);
+        var_dump($result);
+    }
+
+    public static function getCustomerFavoriteProducts() {
+        $objQuery       = SC_Query_Ex::getSingletonInstance();
+
+        $objQuery->setOrder('customer_id, product_id, target_id');
+        $arrReturn = $objQuery->select('*', 'dtb_customer_favorite_products');
+
+        return $arrReturn;
+    }
+}
