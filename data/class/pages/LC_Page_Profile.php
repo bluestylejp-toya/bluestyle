b90b830fcf3454a2093e78cf6132008821501744
Seasoft <info@seasoft.jp.net>
2021-11-18 19:20:04 +0900

    CHAIN-315 プロフィール編集後に、まだ編集画面が出ている
    「出品中アイテム」「リクエストを押しているアイテム」を削除。
    https://bluestyle.backlog.jp/view/CHAIN-315#comment-127399994

--- data/class/pages/LC_Page_Profile.php
+++ data/class/pages/LC_Page_Profile.php
@@ -14,6 +14,10 @@ class LC_Page_Profile extends LC_Page_Ex
     public function init()
     {
         parent::init();
+
+        $masterData = new SC_DB_MasterData_Ex();
+
+        $this->arrPref = $masterData->getMasterData('mtb_pref');
     }
 
     /**
@@ -37,32 +41,5 @@ class LC_Page_Profile extends LC_Page_Ex
     {
         $customer_id = $_REQUEST['customer_id'];
         $this->arrCustomer = SC_Helper_Customer_Ex::sfGetCustomerData($customer_id);
-        $this->arrCustomerProducts = SC_Helper_Customer_Ex::getCustomerProducts($customer_id);
-        $this->arrFavoriteProducts = $this->getFavoriteProducts($customer_id);
-
-        $masterData = new SC_DB_MasterData_Ex();
-        $this->arrPref = $masterData->getMasterData('mtb_pref');
-    }
-
-    /**
-     * 指定された会員のお気に入り商品を取得する
-     *
-     * @return array
-     */
-    public function getFavoriteProducts($customer_id)
-    {
-        $objQuery = SC_Query_Ex::getSingletonInstance();
-        $objProduct = new SC_Product_Ex();
-
-        $objQuery->setWhere('EXISTS(SELECT * FROM dtb_customer_favorite_products WHERE customer_id = ? AND product_id = alldtl.product_id)', [$customer_id]);
-        $objQuery->andWhere('alldtl.del_flg = 0');
-        $objQuery->andWhere('alldtl.status = 1');
-        $objQuery->setOrder('alldtl.product_id DESC');
-        $objQuery->setLimit(10);
-
-        $addCols = ['count_of_favorite'];
-        $arrProducts = $objProduct->lists($objQuery, [], $addCols);
-
-        return $arrProducts;
     }
 }
